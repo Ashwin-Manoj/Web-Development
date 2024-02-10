dd6e9eb72d1d24767140424dd5d11fcc8fc35e80
Ashwin-Manoj <achuashwinmac@gmail.com>
2024-02-10 20:30:55 +0530

    mysql connected

--- Node with SQL/app.js
+++ Node with SQL/app.js
@@ -1,4 +1,24 @@
-const { faker } = require('@faker-js/faker');
+const { faker } = require("@faker-js/faker");
+const mysql = require("mysql2");
+
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "app",
+  password: "ashwinmanoj",
+});
+
+try {
+  connection.query("show tables", (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  });
+} catch (err) {
+  console.log(err);
+}
+
+connection.end();
 
 let getRandomUser = () => {
   return {
@@ -7,6 +27,6 @@ let getRandomUser = () => {
     email: faker.internet.email(),
     password: faker.internet.password(),
   };
-}
+};
 
-console.log(getRandomUser());
+// console.log(getRandomUser());
